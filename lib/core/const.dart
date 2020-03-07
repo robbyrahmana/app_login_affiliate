b11488fc2a89d684b9ddb7ae72793b6992f56111
robbyrahmana <robbyrahmana79@gmail.com>
2020-03-07 13:07:25 +0700

    Basic code

---
+++ lib/core/const.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Color(0XFF222832);
+  static const darkblack = Color(0XFF1f232b);
+  static const lightblack = Color(0XFF2d333d);
+  static const white = Color(0XFFFDFEFE);
+  static const blue = Color(0XFF295fc6);
+  static const darkblue = Color(0XFF214ea3);
+  static const lightblue = Color(0XFF0075ff);
+  static const red = Color(0XFFef3d72);
+}
